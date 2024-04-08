31bdb79a62d30943e6d31227e1bccda452ad1674
W0ggi <voron529403@mail.ru>
2024-04-08 16:22:17 +0300

    authorization/registration db logic

--- pages/RegistrationPage.xaml.cs
+++ pages/RegistrationPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ByWoggi.classes;
+using ByWoggi.pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -29,33 +31,52 @@ namespace ByWoggi
 
         private void FullyRegButton_Click(object sender, RoutedEventArgs e)
         {
+            string login = LoginTextBox.Text;
+            string pwd = PasswordTextbox.Password;
+            string email = EmailTextBox.Text;
             // Проверка валидности логина
-            if (!Regex.IsMatch(LoginTextBox.Text, @"^[a-zA-Z0-9]{3,}$"))
+            if (!Regex.IsMatch(login, @"^[a-zA-Z0-9]{3,}$"))
             {
                 MessageBox.Show("Логин должен содержать минимум 3 символа, без пробелов, русских букв и спецсимволов");
                 return;
             }
             // Проверка валидности пароля
-            else if (!Regex.IsMatch(PasswordTextbox.Password, @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).{7,}$"))
+            else if (!Regex.IsMatch(pwd, @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).{7,}$"))
             {
                 MessageBox.Show("Пароль должен содержать минимум 7 символов с заглавными и маленькими буквами и спецсимволами, без пробелов и русских букв!");
                 return;
             }
             // Проверка идентичности паролей
-            else if (PasswordTextbox.Password != PasswordTextboxRetry.Password)
+            else if (pwd != PasswordTextboxRetry.Password)
             {
                 MessageBox.Show("Пароли должны совпадать!");
                 return;
 
             }
             // Проверка валидность Email
-            else if (!Regex.IsMatch(EmailTextBox.Text, @"^[^@\s]+@(yandex\.ru|mail\.ru|gmail\.com)$"))
+            else if (!Regex.IsMatch(email, @"^[^@\s]+@(yandex\.ru|mail\.ru|gmail\.com)$"))
             {
                 MessageBox.Show("Невалидный Email!");
                 return;
             }
 
-            MessageBox.Show("Регистрация успешна!");
+            // Проверка валидности успешна - процесс регистрации
+            using (var context = new ByWoggiEntities())
+            {
+                var authService = new AuthService(context);
+
+                if (authService.RegisterUser(login, pwd, email))
+                {
+                    MessageBox.Show("Регистрация успешна!");
+                }
+                else
+                {
+                    MessageBox.Show("Пользователь с таким логином или email уже существует");
+                }
+            }
+
+
+
         }
 
     }
