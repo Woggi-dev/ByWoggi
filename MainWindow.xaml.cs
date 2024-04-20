60e95013a39ae1e5d149fef1dee48406cf893300
W0ggi <voron529403@mail.ru>
2024-04-20 23:03:13 +0300

    added UserSession.cs & MainViewModel.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using ByWoggi.pages;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
@@ -24,22 +25,44 @@ namespace ByWoggi
     public partial class MainWindow : Window
     {
         private ByWoggiEntities _context;
+        private UserSession _userSession;
+        private MainViewModel _mainViewModel;
         public MainWindow()
         {
             InitializeComponent();
             _context = new ByWoggiEntities();
+            _userSession = new UserSession();
+            _mainViewModel = new MainViewModel(_userSession);
+            DataContext = _mainViewModel;
+            PrintAllGames();    
+        }
+
+        private void PrintAllGames()
+        {
+            _context = new ByWoggiEntities();
             GameListView.ItemsSource = _context.Games.ToList();
         }
 
+        private void SortGames(string categoryName)
+        {
+            var genreCategoryId = _context.Categories.FirstOrDefault(c => c.name == categoryName)?.category_id;
+            GameListView.ItemsSource = _context.Games.Where(g => g.category_id == genreCategoryId).ToList();
+        }
 
         private void RegistrationLogIn_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            AuthPopup.IsOpen = !AuthPopup.IsOpen;
+            if (!_userSession.IsAuthenticated)
+            {
+                AuthPopup.IsOpen = !AuthPopup.IsOpen;
+                return;
+            }
+            UserPopup.IsOpen = !UserPopup.IsOpen;
         }
 
         private void RegButton_Click(object sender, RoutedEventArgs e)
         {
             CurrentGameContentPage.Content = null;
+            AuthPopup.IsOpen = !AuthPopup.IsOpen;
             RegistrationContentFrame.Navigate(new Uri("pages/RegistrationPage.xaml", UriKind.Relative));
         }
 
@@ -49,6 +72,7 @@ namespace ByWoggi
             RegistrationContentFrame.Content = null;
             CurrentGameContentPage.Content = null;
             GameListView.Visibility = Visibility.Visible;
+            PrintAllGames();
 
         }
 
@@ -56,19 +80,23 @@ namespace ByWoggi
         {
             using (var context = new ByWoggiEntities())
             {
-                var authService = new AuthService(context);
+                var authService = new AuthService(_context);
+                var user = authService.AuthenticateUser(LoginTextbox.Text, PasswordTextbox.Password);
 
-                // Вход пользователя
-                if (authService.AuthenticateUser(LoginTextbox.Text, PasswordTextbox.Password))
-                {
-                    MessageBox.Show("Вход успешен!");
-                }
-                else
+                if (user == null)
                 {
                     MessageBox.Show("Неверный логин или пароль");
+                    return;
                 }
 
+                _mainViewModel.userSession.SignIn(user);
+                RegistrationContentFrame.Content = null;
+                AuthPopup.IsOpen = false;
+
             }
+
+
+            MessageBox.Show("Вход успешен!");
         }
 
         private void CurrentGame_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -77,12 +105,88 @@ namespace ByWoggi
             var stackPanel = (StackPanel)sender;
             var game = (Game)stackPanel.DataContext;
 
-            // Создаем экземпляр страницы с параметрами
             var gameContentPage = new CurrentGameContentPage(game.imagePath, game.name, game.description, game.release_date);
 
-            // Теперь устанавливаем созданный экземпляр страницы в Frame
-            CurrentGameContentPage.Content = gameContentPage; // CurrentGameContentFrame это имя Frame в вашем XAML
+            CurrentGameContentPage.Content = gameContentPage;
 
+        }
+
+        private void SortByActionGames(object sender, RoutedEventArgs e)
+        {
+            SortGames("Экшен");
+        }
+
+        private void SortByRPGGames(object sender, RoutedEventArgs e)
+        {
+            SortGames("RPG");
+        }
+
+        private void SortByStrategyGames(object sender, RoutedEventArgs e)
+        {
+            SortGames("Стратегии");
+        }
+
+        private void SortByAdventureGames(object sender, RoutedEventArgs e)
+        {
+            SortGames("Приключения");
+        }
+
+        private void SortBySimulationGames(object sender, RoutedEventArgs e)
+        {
+            SortGames("Симуляторы");
+
+        }
+
+        private void SortByRaceGames(object sender, RoutedEventArgs e)
+        {
+            SortGames("Гонки");
+
+        }
+
+        private void SortByIndieGames(object sender, RoutedEventArgs e)
+        {
+            SortGames("Инди");
+
+        }
+
+        private void SortByCasualGames(object sender, RoutedEventArgs e)
+        {
+            SortGames("Казуальные");
+
+        }
+
+        private void SortBySportGames(object sender, RoutedEventArgs e)
+        {
+            SortGames("Спортивные");
+
+        }
+
+        private void SearchTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                string searchText = SearchTextbox.Text;
+                var game = _context.Games.FirstOrDefault(g => g.name.Contains(searchText));
+                if (game != null)
+                {
+                    GameListView.Visibility = Visibility.Collapsed;
+                    CurrentGameContentPage.Navigate(new CurrentGameContentPage(game.imagePath, game.name, game.description, game.release_date));
+                }
+                else
+                {
+                    MessageBox.Show("Информация не найдена.");
+                }
+            }
+        }
+
+        private void SearchTextbox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            SearchTextbox.Text = string.Empty;
+        }
+
+        private void SearchTextbox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            SearchTextbox.Text = "Поиск...";
         }
     }
 }
---
+++ classes/MainViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ByWoggi.classes
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public UserSession userSession; 
+
+        // Свойство для отображения имени пользователя или кнопок "Войти/Регистрация"
+        private string _userDisplay;
+
+        public string UserDisplay
+        {
+            get { return _userDisplay; }
+            set
+            {
+                _userDisplay = value;
+                OnPropertyChanged(nameof(UserDisplay));
+            }
+        }
+
+
+        // Конструктор
+        public MainViewModel(UserSession userSession)
+        {
+            // Создаем сессию пользователя и подписываемся на событие изменения статуса входа
+            this.userSession = userSession;
+            this.userSession.OnLoginStatusChanged += UpdateUI;
+            UpdateUI();
+        }
+
+        // Метод для обновления интерфейса
+        private void UpdateUI()
+        {
+            if (userSession.IsAuthenticated)
+            {
+                UserDisplay = userSession.CurrentUser.login; // Имя пользователя для отображения
+            }
+            else
+            {
+                UserDisplay = "Авторизация"; // Текст кнопок для отображения
+            }
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
